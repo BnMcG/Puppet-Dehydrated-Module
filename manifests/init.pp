8c70d379e1a022b6da45a21935fe278d9029332b
Ben Magee <ben@bmagee.com>
2017-11-05 14:38:30 +0000

    Include

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,6 +5,11 @@ class dehydrated (
   String  $home_directory
 ) {
   # Include the classes required to setup Dehydrated
+  include dehydrated::user
+  include dehydrated::repository
+  include dehydrated::filesystem
+  include dehydrated::domains
+
   Class['dehydrated::user'] ->
   Class['dehydrated::repository'] ->
   Class['dehydrated::filesystem'] ->
