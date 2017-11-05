045cdaf4d23b931ce81e305e113273793af30f53
Ben Magee <ben@bmagee.com>
2017-11-05 13:34:14 +0000

    Manage repository permissions recursively

--- manifests/filesystem.pp
+++ manifests/filesystem.pp
@@ -2,6 +2,7 @@ class dehydrated::filesystem {
   file { $dehydrated::dehydrated_home:
     ensure => directory,
     owner => $dehydrated::user,
-    group => 'nogroup'
+    group => 'nogroup',
+    recurse => true
   }
 }
