a651f369da8681d5eb2a9f6ab7bd983507bcac43
Ben Magee <ben@bmagee.com>
2017-11-19 19:32:25 +0000

    Ensure certs directory created

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -11,6 +11,7 @@ define dehydrated::certificate(
   # Change hooks to paths
   case $hook {
     'route53': { $hook_path = "${dehydrated::home_directory}/hooks/route53/route53.sh" }
+    default: { $hook_path = '' }
   }
 
   # Ensure present in domains.txt
@@ -24,10 +25,19 @@ define dehydrated::certificate(
   }
 
   # Custom configuration
+
+  # Ensure that domain directory exists
+  file { "/etc/dehydrated/certs/${domain}/":
+    ensure => directory,
+    owner  => $dehydrated::user,
+    mode   => '0700'
+  }
+
   file { "/etc/dehydrated/certs/${domain}/config":
-    ensure => file,
+    ensure  => file,
     content => epp('dehydrated/config.epp', {
       'hook_path' => $hook_path
-    })
+    }),
+    require => File["/etc/dehydrated/certs/${domain}/"]
   }
 }
--- manifests/config.pp
+++ manifests/config.pp
@@ -2,27 +2,34 @@ class dehydrated::config {
   # Ensure that the domains.txt file is present
   file { "${dehydrated::home_directory}/domains.txt":
     ensure => present,
-    owner => $dehydrated::user,
-    group => $dehydrated::group
+    owner  => $dehydrated::user,
+    group  => $dehydrated::group
   }
 
   # Ensure a hooks directory exists. Any hooks are stored here
   file { "${dehydrated::home_directory}/hooks":
     ensure => directory,
-    owner => $dehydrated::user,
-    group => $dehydrated::group,
+    owner  => $dehydrated::user,
+    group  => $dehydrated::group,
     recurse => true
+  }
+
+  # Ensure that certs directory exists
+  file { "${dehydrated::etc_directory}/certs":
+    ensure => directory,
+    owner => $dehydrated::user,
+    mode  => '0700'
   }
 
   # Ensure that Dehydrated is executable
   file { "${dehydrated::etc_directory}/dehydrated":
     ensure => file,
-    mode => '0755'
-  } ->
+    mode   => '0755'
+  }
 
   # Ensure that Dehydrated is registered with the Let's Encrypt servers
-  exec { "/bin/bash ${dehydrated::etc_directory}/dehydrated --register --accept-terms": 
-    user => $dehydrated::user,
+  -> exec { "/bin/bash ${dehydrated::etc_directory}/dehydrated --register --accept-terms": 
+    user    => $dehydrated::user,
     creates => "${dehydrated::etc_directory}/accounts"
   }
 }
