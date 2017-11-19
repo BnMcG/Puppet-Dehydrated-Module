bc3fd30c8755c6dec9bc35502f8fcfddff47cb84
Ben Magee <ben@bmagee.com>
2017-11-19 20:06:40 +0000

    Linting

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,32 +1,33 @@
+# Class to ensure that Dehydrated is installed correctly
 class dehydrated::install {
   # Ensure the user is created
-  user { $dehydrated::user: 
-    ensure => present,
-    home => $dehydrated::home_directory,
+  user { $dehydrated::user:
+    ensure     => present,
+    home       => $dehydrated::home_directory,
     managehome => true,
-    system => true,
-    shell => '/usr/sbin/nologin'
-  } ->
+    system     => true,
+    shell      => '/usr/sbin/nologin'
+  }
 
   # Ensure the GitHub repository is present, cloned and up-to-date
   # First, ensure Git is installed
-  package { 'git': 
+  -> package { 'git':
     ensure => latest
-  } ->
-  
+  }
+
   # Clone the repository
-  vcsrepo { $dehydrated::etc_directory: 
-    ensure => latest,
+  -> vcsrepo { $dehydrated::etc_directory:
+    ensure   => latest,
     provider => git,
-    source => 'https://github.com/lukas2511/dehydrated.git',
+    source   => 'https://github.com/lukas2511/dehydrated.git',
     revision => 'master'
-  } ->
+  }
 
   # Ensure the Dehydrated install location is correctly permissioned
-  file { $dehydrated::etc_directory:
-    ensure => directory,
-    owner => $dehydrated::user,
-    group => $dehydrated::group,
+  -> file { $dehydrated::etc_directory:
+    ensure  => directory,
+    owner   => $dehydrated::user,
+    group   => $dehydrated::group,
     recurse => true
   }
 }
