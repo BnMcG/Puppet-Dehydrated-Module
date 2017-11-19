aa47ef317a89c03f2938002dd4eab45c56c90ed0
Ben Magee <ben@bmagee.com>
2017-11-19 19:10:55 +0000

    Add domain-specific configuration to certificate

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -1,11 +1,17 @@
 # Class for Let's Encrypt certificate resources
 define dehydrated::certificate(
-  $hook,
-  $challenge,
-  $domain = $title
+  $hook = '',
+  $challenge = 'http-01',
+  $domain = $title,
+  $wellknown = '/var/www/html'
 ) {
 
   include dehydrated
+
+  # Change hooks to paths
+  case $hook {
+    'route53': { $hook = "${dehydrated::home_directory}/hooks/route53/route53.sh" }
+  }
 
   # Ensure present in domains.txt
   concat {"${dehydrated::home_directory}/domains.txt":
@@ -15,5 +21,11 @@ define dehydrated::certificate(
   concat::fragment { 'domain':
     target  => "${dehydrated::home_directory}/domains.txt",
     content => $domain
+  }
+
+  # Custom configuration
+  file { "/etc/dehydrated/certs/${domain}/config":
+    ensure => file,
+    content => epp('dehydrated/config.epp')
   }
 }
