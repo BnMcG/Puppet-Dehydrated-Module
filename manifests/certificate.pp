d146d988e40d1a6974569d6e8ec67338b244db51
Ben Magee <ben@bmagee.com>
2017-11-22 00:30:20 +0000

    Duplicate

--- manifests/certificate.pp
+++ manifests/certificate.pp
@@ -38,7 +38,8 @@ define dehydrated::certificate(
   }
 
   # Run once to generate initial certificate
-  exec { "${dehydrated::etc_directory}/dehydrated --cron --config ${dehydrated::home_directory}/config":
+  exec { "cron-${domain}":
+    command => "${dehydrated::etc_directory}/dehydrated --cron --config ${dehydrated::home_directory}/config",
     user    => $dehydrated::user,
     creates => "${dehydrated::etc_directory}/certs/${domain}/fullchain.pem",
     require => Concat::Fragment['domain']
